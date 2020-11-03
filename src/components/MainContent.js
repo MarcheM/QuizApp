a2dfab09988bec13d4658518ee9d3898b26128cc
Michał Marchewa <michalmarchewa@gmail.com>
2020-11-03 20:16:55 +0100

    update

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MainContent = ({ children }) => {
 
     return (
-        <div style={{ backgroundColor: "#F9F2FA", height: "80vh", width: "100%", maxWidth: '1400px', margin: "0 auto", opacity: '0.5' }}>
+        <div style={{ backgroundColor: "rgba(249, 242, 250, 0.65)", height: "80vh", width: "100%", maxWidth: '1400px', margin: "0 auto" }}>
             {children}
 
         </div>
