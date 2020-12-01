03237d16e65df0adf975e67f4f788dbe4c9babea
Michał Marchewa <michalmarchewa@gmail.com>
2020-12-01 19:03:32 +0100

    save

--- src/components/organisms/QuizCard.js
+++ src/components/organisms/QuizCard.js
@@ -23,9 +23,9 @@ const QuizCard = ({ protoQuiz, singleQuiz, quesNumber, children }) => {
             <div style={{ width: "50%", padding: "10px", backgroundColor: '#b0a' }}>{protoQuiz.category.toUpperCase()} </div>
             <div style={{ width: "50%", padding: "10px", backgroundColor: chooseColor() }}> {protoQuiz.diff.toUpperCase()}</div>
         </div>
-            <div style={{ backgroundColor: "white", width: "80%", margin: "1em auto", padding: "1em" }}>
-                <h2 style={{ borderBottom: "2px solid #eee", padding: "16px", border: "1px solid #333", boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)", borderRadius: "32px", width: "80%", margin: "20px auto 0 auto" }}>{singleQuiz[quesNumber].task}</h2>
-                <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: "space-around", borderRadius: "16px" }}>
+            <div style={{ width: "80%", margin: "1em auto", padding: "1em" }}>
+                <div style={{ backgroundColor: "white", fontSize: "2em", borderBottom: "2px solid #eee", padding: "16px", border: "1px solid #333", boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)", borderRadius: "32px", width: "80%", margin: "1em auto 2.5em auto" }}>{singleQuiz[quesNumber].task}</div>
+                <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: "space-between", borderRadius: "16px" }}>
                     {children}
                 </div>
             </div>
